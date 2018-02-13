6c07ef505d4e59874eed21a7a92ed4ea17849606
develop.rahimi95 <666110gg>
2018-02-13 11:28:36 +0330

    deleted some unused folder

--- app/src/main/java/ir/orangehat/movieinfo/bussines/persistance/database/MovieDatabaseHelper.java
+++ app/src/main/java/ir/orangehat/movieinfo/bussines/persistance/database/MovieDatabaseHelper.java
@@ -14,6 +14,7 @@ import ir.orangehat.movieinfo.bussines.persistance.database.dao.MovieDao;
  * MovieDatabaseHelper
  */
 
+
 public class MovieDatabaseHelper extends BaseDatabaseHelper {
 
     private MovieDao movieDao;
